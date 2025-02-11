5f91745f43caeacfcef8afc3fdaf875c102de4b8
AMAN GUPTA <153358998+amangupta9454@users.noreply.github.com>
2025-02-11 18:21:05 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,10 @@ export default {
   theme: {
     extend: {
       keyframes: {
-        
+        float: {
+          "0%, 100%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-8px)" },
+        },
         tada: {
           '0%': { transform: 'scale(1)' },
           '10%, 20%': { transform: 'scale(0.9) rotate(-3deg)' },
@@ -21,19 +24,13 @@ export default {
           '0%': { opacity: 0, transform: 'translateY(-20px)' },
           '100%': { opacity: 1, transform: 'translateY(0)' },
         },
-        slideIn: {
-          "0%": { opacity: "0", transform: "translateY(30px)" },
-          "100%": { opacity: "1", transform: "translateY(0)" },
-        },
       },
       animation: {
         tada: 'tada 3s ease-in-out infinite',
         'fade-in': 'fadeIn 0.5s ease-in-out',
-        'spin-slow': 'spin 10s linear infinite',
-        slideIn: "slideIn 1s ease-out",
+        float: "float 3s ease-in-out infinite",
       },
     },
   },
   plugins: [],
 }
-
